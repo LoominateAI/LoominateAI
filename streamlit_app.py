76f183371fc86ef0d4444d998c36c72ce7292040
LoominateAI <157995604+LoominateAI@users.noreply.github.com>
2024-01-30 20:54:12 -0500

    Emails streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,12 @@
 import streamlit as st
 import requests
-from transformers import pipeline
+from summarizer import Summarizer
 import newspaper
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import smtplib
 import spacy
+import re
 
 class EmailSender:
     def __init__(self, email_from, email_to, passwd):
@@ -25,19 +26,25 @@ class EmailSender:
         s.sendmail(self.email_from, self.email_to, msg.as_string())
         s.quit()
 
-@st.cache(allow_output_mutation=True)  # Allow mutation for model loading
-def load_models():
-    nlp_model = spacy.load("en_core_web_sm")
-    summarizer = pipeline("summarization", model="facebook/bart-large-cnn", tokenizer="facebook/bart-large-cnn")  # Load BART model
-    return nlp_model, summarizer
+@st.cache(hash_funcs={"MyUnhashableClass": lambda _: None})
+def load_spacy_model():
+    return spacy.load("en_core_web_sm")
+
+@st.cache(hash_funcs={"MyUnhashableClass": lambda _: None})
+def load_bert_summarizer():
+    return Summarizer('bert-large-uncased')
+
+def is_valid_email(email):
+    pattern = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+    return re.match(pattern, email)
 
 def clean_and_extract_informative(text, nlp):
     doc = nlp(text)
-    informative_paragraphs = [p.text.strip() for p in doc.sents if p and not p.text.startswith(("Sign up", "Subscribe", "Download the app"))]
+    informative_paragraphs = [p.text.strip() for p in doc.sents if p and not p.text.startswith(("By", "Sign up", "Subscribe", "Download the app"))]
     return ' '.join(informative_paragraphs)
 
-def summarize_with_bart(text, summarizer):
-    summary = summarizer(text, max_length=2000, min_length=50, do_sample=False)[0]['summary_text']  # Adjust parameters as needed
+def bert_extractive_summarize(text, summarizer):
+    summary = summarizer(text)
     return summary
 
 def get_news(api_key, category):
@@ -51,33 +58,16 @@ def summarize_articles(api_key, category, email_to, email_sender, nlp, summarize
     if headlines:
         summaries = []
         for idx, article_data in enumerate(headlines[:5], start=1):
-            # Option 1: Skipping problematic articles
+            st.write(f"{idx}. {article_data['title']}")
             try:
                 article = newspaper.Article(article_data['url'])
                 article.download()
                 article.parse()
-
-                if not article.text:
-                    st.warning(f"Article {idx} is empty. Skipping...")
-                    continue
-
                 cleaned_text = clean_and_extract_informative(article.text, nlp)
-                summary = summarize_with_bart(cleaned_text, summarizer)
+                summary = bert_extractive_summarize(cleaned_text, summarizer)
                 summaries.append(summary)
-
             except Exception as e:
-                st.warning(f"Unable to read article {idx}. Error: {str(e)}")
-
-            # Option 2: Alternative summarization method
-            # Uncomment this block and comment out the above try-except block if using this option
-            # try:
-            #     article = newspaper.Article(article_data['url'])
-            #     article.download()
-            #     article.parse()
-            #     summary = summarize_with_bart(article.text, summarizer)
-            #     summaries.append(summary)
-            # except Exception as e:
-            #     st.warning(f"Unable to read article {idx}. Error: {str(e)}")
+                st.warning(f"Unable to read article. Error: {str(e)}")
 
         combined_summary = "\n\n".join(summaries)
         st.write("Combined Summary:")
@@ -85,10 +75,10 @@ def summarize_articles(api_key, category, email_to, email_sender, nlp, summarize
 
         html_content = f"""
         <html>
-         <body>
-          <h1>Daily News Summary</h1>   
-          <p>{combined_summary}</p>  
-         </body>
+          <body>
+            <h1>Daily News Summary</h1>     
+            <p>{combined_summary}</p>   
+          </body>
         </html>
         """
         email_sender.send_email('Daily News', html_content)
@@ -96,12 +86,10 @@ def summarize_articles(api_key, category, email_to, email_sender, nlp, summarize
     else:
         st.warning(f"No valid headlines found for category {category}.")
 
-
-
 st.title("News Headlines App")
 email_to = st.text_input("Enter your email:")
 if st.button("Submit"):
-    if email_to:
+    if is_valid_email(email_to):
         st.success(f"Email submitted: {email_to}")
     else:
         st.warning("Please enter a valid email.")
@@ -113,14 +101,13 @@ if st.button("Summarize News Headlines"):
     email_from = st.secrets["email_from"]
     passwd = st.secrets["passwd"]
 
-    if api_key and selected_categories and email_to:
-        nlp_model, summarizer_model = load_models()
+    if api_key and selected_categories and is_valid_email(email_to):
+        nlp_model = load_spacy_model()
+        summarizer_model = load_bert_summarizer()
 
         email_sender = EmailSender(email_from, email_to, passwd)
         for category in selected_categories:
             summarize_articles(api_key, category, email_to, email_sender, nlp_model, summarizer_model)
             st.write("---")
-
     else:
         st.warning("Please provide a valid email and select at least one category.")
-
